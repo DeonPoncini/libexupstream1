6ce3897c3c09748e8373bb9bc15d34391a78f796
Deon Poncini <dex1337@gmail.com>
2014-11-29 18:27:49 -0800

    Simple upstream library
    Built with the new build system and exporting

---
+++ include/upstream1/upstream1.h
@@ -0,0 +1,11 @@
+#ifndef UPSTREAM1_H
+#define UPSTREAM1_H
+
+namespace upstream1
+{
+
+int upstream_version();
+
+}
+
+#endif
---
+++ src/upstream1.cpp
@@ -0,0 +1,11 @@
+#include "upstream1/upstream1.h"
+
+namespace upstream1
+{
+
+int upstream_version()
+{
+    return 1;
+}
+
+}
